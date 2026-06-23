9ebdad63e4ea267a2173502ac03ac09459d72af0
Medaltus <hello@medaltus.com>
2026-06-23 15:56:21 -0400

    Fix typo in header 'skus' to 'sku'

--- api/cron/dedup-orders.js
+++ api/cron/dedup-orders.js
@@ -16,7 +16,7 @@ const HEADERS = [
   'order_id', 'date', 'status', 'order_total',
   'promotion_ids', 'is_premium_order', 'promotion_discount',
   'item_price', 'quantity_ordered', 'quantity_shipped',
-  'unit_count', 'skus', 'brand', 'last_updated',
+  'unit_count', 'sku', 'brand', 'last_updated',
 ];
 
 module.exports = async (req, res) => {
