eaeec78a92f0aef818243e9b93768038586e8325
Medaltus <hello@medaltus.com>
2026-06-26 11:12:40 -0400

    Change export to module.exports in run-analysis.js

--- api/run-analysis.js
+++ api/run-analysis.js
@@ -15,7 +15,7 @@ const BRAND_DESCRIPTIONS = {
   default: 'a Medaltus brand'
 };
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
